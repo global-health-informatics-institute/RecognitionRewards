71491ae64d2dd5b1e4d9fa30ea8a9d540aefe0de
89Tain <manda.titani@gmail.com>
2025-03-18 16:50:16 +0200

    limit check

--- app/controllers/points_logs_controller.rb
+++ app/controllers/points_logs_controller.rb
@@ -1,19 +1,22 @@
 class PointsLogsController < ApplicationController
   def index
-    @open_contests = Competition.where("end_date >= ? and competition_id in (?)",
+    @open_contests = Competition.where('end_date >= ? and competition_id in (?)',
                                        Date.today,
                                        Participant.where(person_id: Current.user.person_id)
-                                                  .collect{|x|x.competition_id})
-
+                                                  .collect { |x| x.competition_id })
   end
 
   def create
-
     PointsLog.transaction do
       PointsLog.create(competition_id: params[:competition_id], gifter: params[:gifter], receiver: params[:candidate],
                        date_awarded: DateTime.now, points_awarded: params[:points_log][:points_awarded])
       pp = ParticipantPoint.find_by_competition_id_and_person_id(params[:competition_id], Current.user.person_id)
       pp.points_remaining -= params[:points_log][:points_awarded].to_i
+      if pp.points_remaining < 0
+        flash[:error] = 'You do not have enough points to award'
+        redirect_to "/competitions/#{params[:competition_id]}"
+        return
+      end
       pp.save
     end
     redirect_to "/competitions/#{params[:competition_id]}"
@@ -35,9 +38,7 @@ class PointsLogsController < ApplicationController
   end
 
   def destroy
-
   end
 
   private
-
 end
