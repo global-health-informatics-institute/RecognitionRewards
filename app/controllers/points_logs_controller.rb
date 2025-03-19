f58cf6ae8c7bada45aa08135b3cc6d9d85190bf3
89Tain <manda.titani@gmail.com>
2025-03-19 15:26:17 +0200

    Add validation for points awarded to prevent negative and zero values, and prevent self-gifting

--- app/controllers/points_logs_controller.rb
+++ app/controllers/points_logs_controller.rb
@@ -11,8 +11,24 @@ class PointsLogsController < ApplicationController
       PointsLog.create(competition_id: params[:competition_id], gifter: params[:gifter], receiver: params[:candidate],
                        date_awarded: DateTime.now, points_awarded: params[:points_log][:points_awarded])
       pp = ParticipantPoint.find_by_competition_id_and_person_id(params[:competition_id], Current.user.person_id)
+      if params[:candidate] == params[:gifter]
+        flash[:alert] = 'Smart!! But you cannot gift yourself'
+        redirect_to new_points_log_path and return
+      end
       if pp.points_remaining < params[:points_log][:points_awarded].to_i
         flash[:alert] = 'You do not have enough points to award'
+        redirect_to new_points_log_path and return
+      end
+      if params[:points_log][:points_awarded].to_i.negative?
+        flash[:alert] = 'You can not gift negative points'
+        redirect_to new_points_log_path and return
+      end
+      if params[:points_log][:points_awarded].to_i.zero?
+        flash[:alert] = 'You can not gift nothing'
+        redirect_to new_points_log_path and return
+      end
+      if params[:candidate] == params[:gifter]
+        flash[:alert] = 'Smart!! But you cannot gift yourself'
         redirect_to new_points_log_path and return
       end
       pp.points_remaining -= params[:points_log][:points_awarded].to_i
