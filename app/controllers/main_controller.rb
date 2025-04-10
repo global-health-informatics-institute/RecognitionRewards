b40a00b6fa3f42fea185f9fe75efc8e9664e1de5
89Tain <manda.titani@gmail.com>
2025-04-10 15:20:44 +0200

    Add user-specific points display and enhance leaderboard layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
 
   def index
+    puts("CURRENT USER: #{Current.user.user_id}")
     elections = Competition.where("start_date <= ? AND end_date >= ? and competition_type = ? ",
                                   Date.today, Date.today, 'Election').order(:start_date)
     contests = Competition.where("end_date >= ? and competition_id in (?)",
@@ -9,11 +10,14 @@ class MainController < ApplicationController
                                                                            .collect{|x|x.competition_id})
 
     @current_competitions = elections + contests
-
-
     @ranks = {}
     @current_competitions.each do |competition|
-      @ranks[competition.id] = competition.pretty_ranking(number=3)
+      @ranks[competition.id] = {
+        rank: competition.pretty_ranking(3),
+        total_points: competition.all_points_votes,
+      }
+      @remaining_points = ParticipantPoint.where(person_id: Current.user.user_id,
+                                                 competition_id: competition.id).pluck(:points_remaining).first || 0
     end
   end
 end
